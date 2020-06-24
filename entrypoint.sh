072135c87e3a79d7c1797abd8db9e8d26afebeb3
seraf <serafeimkourlos@gmail.com>
2020-06-24 21:09:08 +0300

    Improve project config

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 # You can set here anything you need before starting the spring boot application
-mvn clean install spring-boot:run
+mvn spring-boot:run
--- src/main/java/com/example/application/docker/demo/HomeController.java
+++ src/main/java/com/example/application/docker/demo/HomeController.java
@@ -6,8 +6,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HomeController {
 
-    @RequestMapping("")
+    @RequestMapping("/")
     public String home(){
-        return "Hello Sera";
+        return "Spring Boot Home";
     }
 }
