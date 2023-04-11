02c17574b8b14fee3136304b6ae338566dcf317c
d-donia <d.donia@studenti.unisa.it>
2023-04-11 23:21:45 +0200

    edit pom

--- src/main/java/com/example/application/docker/demo/HomeController.java
+++ src/main/java/com/example/application/docker/demo/HomeController.java
@@ -3,11 +3,23 @@ package com.example.application.docker.demo;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Arrays;
+import java.util.List;
+import org.apache.commons.collections4.CollectionUtils;
+
+import java.util.Arrays;
+import java.util.List;
+
 @RestController
 public class HomeController {
 
     @RequestMapping("/")
     public String home(){
-        return "Spring Boot Home";
+        String[] list1 = {"A", "C"};
+        String[] list2 = {"B", "D"};
+        List<String> l1 = Arrays.asList(list1);
+        List<String> l2 = Arrays.asList(list2);
+        List<String> mergedList = CollectionUtils.collate(l1, l2);
+        return "List1: " + l1 + "\nList2: " + l2 + "\nMerged list: " + mergedList;
     }
 }
